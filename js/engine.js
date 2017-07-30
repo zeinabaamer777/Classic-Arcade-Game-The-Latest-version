9e98096497c65401bc825a8268f3d55290a07fb1
Zeinab <zeinabaamer157@gmail.com>
2017-07-31 00:35:42 +0200

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -4,7 +4,6 @@
  * 10/28/2015
  * Udacity
  */
-
 var Engine = (function(global) {
     /* Predefine the variables we'll be using within this scope,
      * create the canvas element, grab the 2D context for that canvas
@@ -15,17 +14,17 @@ var Engine = (function(global) {
         canvas = doc.createElement('canvas'),
         ctx = canvas.getContext('2d'),
         lastTime;
-		Score = doc.createElement('div');
-		// Life = doc.createElement('div');
-		
+    Score = doc.createElement('div');
+    // Life = doc.createElement('div');
+
     canvas.width = 505;
     canvas.height = 606;
     doc.body.appendChild(canvas);
-	doc.body.appendChild(Score);
-	Scorecontent = doc.createTextNode('Score = ');
-	Scoreupdate = doc.createTextNode(player.score);
-	Score.appendChild(Scorecontent);
-	Score.appendChild(Scoreupdate);
+    doc.body.appendChild(Score);
+    Scorecontent = doc.createTextNode('Score = ');
+    Scoreupdate = doc.createTextNode(player.score);
+    Score.appendChild(Scorecontent);
+    Score.appendChild(Scoreupdate);
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
      */
@@ -44,7 +43,7 @@ var Engine = (function(global) {
          */
         update(dt);
         render();
-		Scoreupdate.nodeValue = player.score;
+        Scoreupdate.nodeValue = player.score;
         /* Set our lastTime variable which is used to determine the time delta
          * for the next time this function is called.
          */
@@ -105,12 +104,12 @@ var Engine = (function(global) {
          * for that particular row of the game level.
          */
         var rowImages = [
-                'images/water-block.png',   // Top row is water
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
-                'images/grass-block.png'    // Row 2 of 2 of grass
+                'images/water-block.png', // Top row is water
+                'images/stone-block.png', // Row 1 of 3 of stone
+                'images/stone-block.png', // Row 2 of 3 of stone
+                'images/stone-block.png', // Row 3 of 3 of stone
+                'images/grass-block.png', // Row 1 of 2 of grass
+                'images/grass-block.png' // Row 2 of 2 of grass
             ],
             numRows = 6,
             numCols = 5,
